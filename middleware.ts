4e3aa097a08b93f0b68468381737b6f8a4470ea0
Gabriel <coldcosmas@gmail.com>
2026-05-30 17:31:26 -0400

    fix: export actual middleware function for NextAuth

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-// middleware.ts
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/admin/:path*"],  // protect everything under /admin except the login page
-};
