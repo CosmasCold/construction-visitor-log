b2326d14185243bf939800f654d02752c1088201
Gabriel <coldcosmas@gmail.com>
2026-06-06 10:17:54 -0400

    update csp

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
               "img-src 'self' https://images.unsplash.com data: https://saasdb.net",
               "connect-src 'self' https://api.brevo.com https://api.stripe.com https://client.crisp.chat wss://client.relay.crisp.chat",
               "frame-src https://checkout.stripe.com https://js.stripe.com",
-              "font-src 'self' https://client.crisp.chat",
+              "font-src 'self' https://client.crisp.chat data:",
               "object-src 'none'",
               "base-uri 'self'",
               "form-action 'self'",
