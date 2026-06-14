42c162804ce43e9214220bc639b7a883ee4d27e3
Gabriel <coldcosmas@gmail.com>
2026-06-14 11:00:49 -0400

    Add data files for industries and cities

---
+++ data/industries.ts
@@ -0,0 +1,27 @@
+export const industries = [
+  {
+    slug: "construction",
+    name: "Construction",
+    description: "construction sites and job sites",
+  },
+  {
+    slug: "warehouse",
+    name: "Warehouse",
+    description: "warehouses and distribution centers",
+  },
+  {
+    slug: "office",
+    name: "Office",
+    description: "corporate offices and coworking spaces",
+  },
+  {
+    slug: "manufacturing",
+    name: "Manufacturing",
+    description: "manufacturing plants and factories",
+  },
+  {
+    slug: "logistics",
+    name: "Logistics",
+    description: "logistics hubs and shipping facilities",
+  },
+];
