1b9164865c519b563a7149739d14ac57c2deb875
Gabriel <coldcosmas@gmail.com>
2026-06-27 16:45:08 -0400

    site optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -234,34 +234,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        // app/layout.tsx — inside <head>
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      _linkedin_partner_id = "10468409";
-      window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-      window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-      (function(l) {
-        if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-        window.lintrk.q=[]}
-        var s = document.getElementsByTagName("script")[0];
-        var b = document.createElement("script");
-        b.type = "text/javascript";b.async = true;
-        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-        s.parentNode.insertBefore(b, s);
-      })(window.lintrk);
-    `,
-  }}
-/>
-<noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: "none" }}
-    alt=""
-    src="https://px.ads.linkedin.com/collect/?pid=10468409&fmt=gif"
-  />
-</noscript>
+        
         <link rel="preconnect" href="https://saasdb.net" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fazier.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://www.saashub.com" crossOrigin="anonymous" />
